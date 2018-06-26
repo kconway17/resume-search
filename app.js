79eb69675c4dd9f252ba269ca07f06117683464e
kconway <kconway@PCI-134.pcism.com>
2018-06-26 11:58:32 -0400

    changes in js files

--- app.js
+++ app.js
@@ -1,202 +1,95 @@
-var async = require('async');
-var express = require('express');
-var bodyParser = require('body-parser');
-var r = require('rethinkdb');
+var express = require('express')
+  , app = express()
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , server = require('http').createServer(app)
+  , util = require('util')
+  , db = require('./lib/resume_db.js');
 
-var config = require('config');
-var app = express();
+app.use(express.static('public'));
+app.use(cookieParser());
 
-var submit 
-
-
-//For serving the index.html and all the other front-end assets.
-app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-//The REST routes for "resumes".
-app.route('/resumes')
-  .get(listAllResumes)
-  .post(createResume);
+// set up the RethinkDB database
+db.setup();
 
-app.route('/resumes/:id')node
-  .get(getResumeItem)
-  .put(updateResumeItem)
-  .delete(deleteResumeItem);
 
-//If we reach this middleware the route could not be handled and must be unknown.
-app.use(handle404);
+app.get('/', 
+  function (req, res) {
+    res.redirect('/addResume.html');
+  }
+)
 
-//Generic error handling middleware.
-app.use(handleError);
+app.get('/resume', 
+  function (req, res) {
+    res.redirect('/addResume.html');
+  }
+)
 
-/*
- * Retrieve all resume items.
- */
-function listAllResumes(req, res, next) {
-  r.table('resumes').orderBy({index: 'createdAt'}).run(req.app._rdbConn, function(err, cursor) {
-    if(err) {
-      return next(err);
-    }
-
-    //Retrieve all the resumes in an array.
-    cursor.toArray(function(err, result) {
+app.post('/resume', function(req, res){
+	console.log("POST /resume");
+	console.log( req.body );
+		
+  // Saving the new user to DB
+  db.saveResume({
+	  //TODO:  add additional fields here from form
+      user_name: req.body.username,
+      work_role: req.body['Work Role'],
+	  clearance_1_month: req.body.clearance_1_month,
+	  clearance_1_year: req.body.clearance_1_year,
+	  company: req.body.Company,
+	  degree: req.body.Degree,
+	  advanced_degree: req.body['Advanced Degree'],
+	  experience: req.body.Experience,
+	  technical: req.body.Technical,
+	  degree_field: req.body.degree_field,
+	  advanced_degree_field: req.body.advanced_degree_field,
+	  certification: req.body.Certification,
+	  cert_1_month: req.body.cert_1_month,
+	  cert_1_year: req.body.cert_1_year,
+	  training: req.body.Training,
+	  training_1_month: req.body.training_1_month,
+	  training_1_year: req.body.training_1_year,
+	  executive_summary: req.body.executive_summary,
+	  work_history: req.body['Work History'],
+	  work_1_from_month: req.body.work_1_from_month,
+	  work_1_from_year: req.body.work_1_from_year,
+	  work_1_description: req.body.work_1_description,
+	  work_1_to_month: req.body.work_1_to_month,
+	  work_1_to_year: req.body.work_1_to_year,
+	tools_experience: req.body['Tools Experience'],
+	keywords: req.body.keywords
+    },
+    function(err, saved) {
+      console.log("[DEBUG][/resume][saveResume] %s", saved);
       if(err) {
-        return next(err);
+        req.write('<h1>ERROR</h1> <p>There was an error creating the account. Please try again later</p>');
+        res.write('<a href="/resume">Add Resume</a>');
+		res.end();
+        return
       }
-
-      res.json(result);
-    });
-  });
-}
-
-/*
- * Insert a new resume item.
- */
-function createResume(req, res, next) {
-  var resumeItem = req.body;
-  resumeItem.createdAt = r.now();
-
-  console.dir(resumeItem);
-
-  r.table('resumes').insert(resumeItem, {returnChanges: true}).run(req.app._rdbConn, function(err, result) {
-    if(err) {
-      return next(err);
+      if(saved) {
+        console.log("[DEBUG][/resume][saveResume] /");
+        res.write('<h1>SUCCESS</h1> <p>Resume Was Stored</p>');
+        res.write('<a href="/resume">Add Resume</a>');
+        res.end();
+      }
+      else {
+        console.log("[ERROR][/resume][saveResume] /");
+        res.write('<h1>ERROR</h1> <p>Resume Was NOT Stored</p>');
+        res.write();
+        res.write('<a href="/resume">Add Resume</a>');
+        res.end();
+      }
+      return
     }
-
-    res.json(result.changes[0].new_val);
-  });
-}
-
-/*
- * Get a specific resume item.
- */
-function getResumeItem(req, res, next) {
-  var resumeItemID = req.params.id;
-
-  r.table('resumes').get(reusmeItemID).run(req.app._rdbConn, function(err, result) {
-    if(err) {
-      return next(err);
-    }
-
-    res.json(result);
-  });
-}
-
-/*
- * Update a resume item.
- */
-function updateResumeItem(req, res, next) {
-  var resumeItem = req.body;
-  var resumeItemID = req.params.id;
-
-  r.table('resumes').get(resumeItemID).update(resumeItem, {returnChanges: true}).run(req.app._rdbConn, function(err, result) {
-    if(err) {
-      return next(err);
-    }
-
-    res.json(result.changes[0].new_val);
-  });
-}
-
-/*
- * Delete a resume item.
- */
-function deleteResumeItem(req, res, next) {
-  var resumeItemID = req.params.id;
-
-  r.table('resumes').get(resumeItemID).delete().run(req.app._rdbConn, function(err, result) {
-    if(err) {
-      return next(err);
-    }
-
-    res.json({success: true});
-  });
-}
-
-/*
- * Page-not-found middleware.
- */
-function handle404(req, res, next) {
-  res.status(404).end('not found');
-}
-
-/*
- * Generic error handling middleware.
- * Send back a 500 page and log the error to the console.
- */
-function handleError(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).json({err: err.message});
-}
-
-/*
- * Store the db connection and start listening on a port.
- */
-function startExpress(connection) {
-  app._rdbConn = connection;
-  app.listen(config.express.port);
-  console.log('Listening on port ' + config.express.port);
-}
-
-/*
- * Connect to rethinkdb, create the needed tables/indexes and then start express.
- * Create tables/indexes then start express
- */
-async.waterfall([
-  function connect(callback) {
-    r.connect(config.rethinkdb, callback);
-  },
-  function createDatabase(connection, callback) {
-    //Create the database if needed.
-    r.dbList().contains(config.rethinkdb.db).do(function(containsDb) {
-      return r.branch(
-        containsDb,
-        {created: 0},
-        r.dbCreate(config.rethinkdb.db)
-      );
-    }).run(connection, function(err) {
-      callback(err, connection);
-    });
-  },
-  function createTable(connection, callback) {
-    //Create the table if needed.
-    r.tableList().contains('resumes').do(function(containsTable) {
-      return r.branch(
-        containsTable,
-        {created: 0},
-        r.tableCreate('resumes')
-      );
-    }).run(connection, function(err) {
-      callback(err, connection);
-    });
-  },
-  function createIndex(connection, callback) {
-    //Create the index if needed.
-    r.table('resumes').indexList().contains('createdAt').do(function(hasIndex) {
-      return r.branch(
-        hasIndex,
-        {created: 0},
-        r.table('resumes').indexCreate('createdAt')
-      );
-    }).run(connection, function(err) {
-      callback(err, connection);
-    });
-  },
-  function waitForIndex(connection, callback) {
-    //Wait for the index to be ready.
-    r.table('resumes').indexWait('createdAt').run(connection, function(err, result) {
-      callback(err, connection);
-    });
-  }
-], function(err, connection) {
-  if(err) {
-    console.error(err);
-    process.exit(1);
-    return;
-  }
-  
-  
-
-  startExpress(connection);
+  );
 });
+
+
+server.listen(8000);
+console.log('[INFO] [app.js]  Resume Application listening on port 8000...');
